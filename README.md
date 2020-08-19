4731814dbc035913ecf7e5b77d324e175339474e
EffortIsSuccess <patrickreiner.schmelzer@gmail.com>
2020-08-19 12:05:52 +0200

    Clean up directory

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Flask
+# Simple to do list using Python / Flash and SQLite as a database
--- SendLogFile.py
+++
@@ -1,9 +0,0 @@
-import os
-import requests
-
-def uploadFile():
-    absolutePath = os.path.dirname(os.path.abspath(__file__))
-    filePath = absolutePath + '\\test.txt'
-    print(filePath)
-    with open(filePath, 'rb') as f:
-        requests.post('http://localhost:5000/upload', data=f)
